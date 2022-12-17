23bbd57fcf7bd60d9a538b5fb5304056aa885cb6
alunduil <alunduil@gmail.com>
2022-12-17 18:04:33 +0000

    Add pyproject.toml tweaks to file conversion.
    closes #33
    
    This adds deep conversions of pyproject.toml.  Likely this will morph as I
    continue playing with the concept to a separate function for pyproject.toml that
    handles the conversion by doing the substitutions per field needed rather than
    wholesale.  This will allow richer transformations to be encoded.

--- templatise/actions.py
+++ templatise/actions.py
@@ -1,11 +1,16 @@
 """File Change Actions."""
 
+import logging
 import pathlib
 import shutil
 import tempfile
 
+import toml
+
 import templatise.configuration as _configuration
 import templatise.licence as _licence
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def convert_file(
@@ -15,7 +20,26 @@ def convert_file(
     with tempfile.NamedTemporaryFile("w") as result:
         result.write(configuration.sub(path.read_text()))
         result.flush()
+        if path.name == "pyproject.toml":
+            _tweak_pyproject_toml(result)
         shutil.copy(result.name, path)
+
+
+def _tweak_pyproject_toml(handle: "tempfile._TemporaryFileWrapper[str]") -> None:
+    """Tweak elements of the pyproject.toml file."""
+    pyproject = toml.load(handle)
+    pyproject["tool"]["poetry"]["classifiers"] = [
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+    ]
+    toml.dump(pyproject, handle)
 
 
 def convert_module(
